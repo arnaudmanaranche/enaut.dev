743569944d8b858cd8feb5f12f35245f1e932fae
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2022-11-18 14:45:00 +0100

    fix: change contact email

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -3,5 +3,5 @@ import type { ReactElement } from 'react'
 import { Button } from './Button'
 
 export const ContactMe = (): ReactElement => (
-  <Button to="mailto:arnaud.mana@gmail.com">Send me an email</Button>
+  <Button to="mailto:hello@enaut.dev">Send me an email</Button>
 )
