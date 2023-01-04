db121768a2477327bd46c7c0257e9389faa5797d
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-01-04 08:28:56 +0100

    feat: generate open graph image

--- src/layout/components/Metadata.tsx
+++ src/layout/components/Metadata.tsx
@@ -20,7 +20,7 @@ export const Metadata = (): ReactElement => {
       <meta name="msapplication-TileColor" content="#FFFFFF" />
       <link rel="canonical" href={`${siteMetadata.siteUrl}${router.asPath}`} />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Braydon Coyer" />
+      <meta property="og:site_name" content={siteMetadata.author} />
       <meta property="og:description" content={siteMetadata.description} />
       <meta property="og:title" content={siteMetadata.title} />
       <meta property="og:image" content={siteMetadata.socialBanner} />
@@ -28,7 +28,7 @@ export const Metadata = (): ReactElement => {
       <meta name="twitter:site" content={siteMetadata.twitterHandle} />
       <meta name="twitter:title" content={siteMetadata.title} />
       <meta name="twitter:description" content={siteMetadata.description} />
-      <meta name="twitter:image" content="" />
+      <meta name="twitter:image" content={siteMetadata.socialBanner} />
     </Head>
   )
 }
