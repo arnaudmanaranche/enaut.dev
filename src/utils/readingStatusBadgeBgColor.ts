57af009fdf1ec6843021c96ac0d7a432bcd05308
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-01-19 08:29:29 +0100

    feat: integration readings index page

---
+++ src/utils/readingStatusBadgeBgColor.ts
@@ -0,0 +1,17 @@
+import type { ReadingStatus } from '@/libs'
+
+export const readingStatusBadgeBgColor = (status: ReadingStatus): string => {
+  switch (status) {
+    case 'to-read':
+      return 'bg-sky-500'
+    case 'currently-reading':
+      return 'bg-green-500'
+    case 'read':
+      return 'bg-rose-500'
+    case 'to-buy':
+      return 'bg-yellow-500'
+    default:
+      console.error(`Status ${status} is not supported`)
+      return 'bg-gray-500'
+  }
+}
