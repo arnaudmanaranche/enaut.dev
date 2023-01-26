e996b5d176ff41feeee12e0b469d68bfd6785786
Eñaut <hello@enaut.dev>
2023-01-26 15:01:17 +0100

    feat: jest setup (#168)
    * feat: setup jest with a test
    
    * feat: run jest on github actions
    
    * feat: add names for job/workflow

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
