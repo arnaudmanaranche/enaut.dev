ca25d3862e44c6dc13145b2435a8a460397cfe1b
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-04-20 15:47:05 +0200

    fix: remove useless scroll

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,9 +4,9 @@ import { Header } from './components/Header'
 
 export const Layout = ({ children }: { children: ReactNode }): ReactElement => {
   return (
-    <div className="mx-auto mb-10 max-w-2xl">
+    <div className="mx-auto max-w-2xl">
       <Header />
-      <main className="min-h-screen px-6 md:px-0">{children}</main>
+      <main className="px-6 md:px-0">{children}</main>
     </div>
   )
 }
