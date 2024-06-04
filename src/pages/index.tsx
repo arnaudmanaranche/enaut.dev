7185930d03b11f5e5de3b0adb61d80e7d8a93539
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-06-04 18:15:20 +0200

    feat: animate enter posts list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,34 @@
 import type { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import type { ReactNode } from 'react'
 
+import AnimateEnter from '@/components/AnimateEnter'
 import type { BlogPostsList } from '@/libs/notion/blog'
 import { getBlogPostsList } from '@/libs/notion/blog'
 import { getMonthAndYearFromDate } from '@/utils/getMonthAndYearFromDate'
 
-const Home = ({ postsList }: { postsList: BlogPostsList[] }) => {
+export interface HomeProps {
+  postsList: BlogPostsList[]
+}
+
+const Home = ({ postsList }: HomeProps): ReactNode => {
   return (
     <>
       <Head>
         <title>enaut.dev | Frontend Engineer</title>
       </Head>
-      <h2 className="mb-5 text-lg">Posts</h2>
-      {postsList.map((post) => (
-        <div key={post.id} className=" text-[#a8a29e] hover:text-[#ffffff]">
-          <Link href={`/blog/${post.slug}`} className="flex justify-between">
-            <span>{post.title}</span>
-            <span>{getMonthAndYearFromDate(new Date(post.createdAt))}</span>
-          </Link>
-        </div>
-      ))}
+      <AnimateEnter>
+        <h2 className="mb-5 text-lg">Posts</h2>
+        {postsList.map((post) => (
+          <div key={post.id} className=" text-[#a8a29e] hover:text-[#ffffff]">
+            <Link href={`/blog/${post.slug}`} className="flex justify-between">
+              <span>{post.title}</span>
+              <span>{getMonthAndYearFromDate(new Date(post.createdAt))}</span>
+            </Link>
+          </div>
+        ))}
+      </AnimateEnter>
     </>
   )
 }
