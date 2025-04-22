1edd1e7de299488c398a29949bf69d49592dcc13
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2025-04-22 18:11:12 +0200

    chore: removed unused env declaration types

--- typings.d.ts
+++ typings.d.ts
@@ -1,10 +1,6 @@
 namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string
-    // Vercel
-    VERCEL_ACCESS_TOKEN: string
-    VERCEL_PROJECT_ID: string
-    VERCEL_WHOOP_REFRESH_TOKEN_ENV_VARIABLE_ID: string
     // Notion
     NOTION_SECRET: string
     BLOG_DATABASE_ID: string
